8bfe2e3b371dbbe2d85e657da316683df371074e
Victor J <103219082+juarezv90@users.noreply.github.com>
2022-05-12 13:54:28 -0400

    Added contact page
    Added contact page, updated links for social platforms

--- Resource/JS/script.js
+++ Resource/JS/script.js
@@ -1,3 +1,7 @@
 const notReady = () => {
     alert("Currently under construction, come check out a little later.")
 }
+
+const tryLater = () => {
+    alert('Under construction, try emailing directly')
+}
