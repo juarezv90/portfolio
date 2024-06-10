39fdbd27c0e2c146081df02d0b4b5ead5838b4f1
Victor Juarez <juarez.v.lpn@gmail.com>
2024-06-10 15:43:09 -0400

    added project modal for display

--- src/assets/ProjectItem.jsx
+++ src/assets/ProjectItem.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import '../scss/project_modal_container.scss'
 
 function ProjectItem({ props }) {
+  const [showProject, setShowProject] = useState(false)
+
+
+  let handleClick = (e)=> {
+    e.preventDefault()
+    setShowProject(true)
+
+  }
+
   return (
+    <>
     <article className="project_item">
       <h4>{props.name}</h4>
       <span className="pub">Published: {props.created_date}</span>
@@ -10,17 +22,47 @@ function ProjectItem({ props }) {
         <p className="details">
           {props.about_project.slice(0, 149)}{" "}
           {props.about_project.length > 150 && "..."}
-          <a href="/">  See More</a>
+          <a onClick={(e) =>handleClick(e)} style={{cursor:"pointer"}}>  Read More</a>
         </p>
       </div>
       <p className="tags">
         Designed using:
-        {props.project_tags.map(item => (
-          <span className="tag">{item} </span>
+        {props.project_tags.map((item, key )=> (
+          <span className="tag" key={item + key}>{item} </span>
         ))}
       </p>
     </article>
+    {showProject && <ShowModal project={props} show={setShowProject} />}
+    </>
   );
 }
 
 export default ProjectItem;
+
+export let ShowModal = ({project, show}) => {
+  console.log(project);
+
+  function handleOnClick() {
+    show(false)
+  }
+
+  return (
+    <div className="modalContainer">
+      <div className="banner" style={{backgroundImage:`url("./project_images/${project.image}.png")`}}></div>
+      <button className="close" onClick={handleOnClick}>x</button>
+        <h4>{project.name}</h4>
+      <div className="tags">
+        <p>Technologies:</p>
+        {project.project_tags.map((tag, key) => (
+          <p key={tag+key}>{tag}</p>
+        ))}
+      </div>
+      <p>{project.about_project}</p>
+      <div className="project_links">
+        <a href={`${project.git_url}`} target="_blank">Git</a>
+        <a href={`${project.active_url}`} target="_blank">Live Project</a>
+      </div>
+      
+    </div>
+  )
+}
